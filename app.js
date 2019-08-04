f71282d38dd23c6e6cb368e999b2ea230c06ccde
Gimena Bernadet <gimena.bernadet@dlocal.com>
2019-08-03 21:31:28 -0300

    #UPDATE cors policy

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/v1', router);
 
